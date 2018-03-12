9f839477b7387f9ca5e8007eb3ec69359ec1cdd6
Harry Tang <harry@powerkernel.com>
2018-03-12 16:33:08 +0700

    update icon

--- memberMenu.php
+++ memberMenu.php
@@ -12,9 +12,9 @@ use common\widgets\SideMenu;
 
 $menu=[
     'title'=>Yii::$app->getModule('billing')->t('Billing'),
-    'icon'=> 'vcard',
+    'icon'=> 'shopping-bag',
     'items'=>[
-        ['icon' => 'address-card-o', 'label' => Yii::$app->getModule('billing')->t('Information'), 'url' => ['/billing/info'], 'active' => Core::checkMCA('billing', 'info', '*')],
+        ['icon' => 'info-square', 'label' => Yii::$app->getModule('billing')->t('Information'), 'url' => ['/billing/info'], 'active' => Core::checkMCA('billing', 'info', '*')],
         ['icon' => 'credit-card', 'label' => Yii::$app->getModule('billing')->t('Invoices'), 'url' => ['/billing/invoice'], 'active' => Core::checkMCA('billing', 'invoice', '*')],
         ['icon' => 'address-book', 'label' => Yii::$app->getModule('billing')->t('Addresses'), 'url' => ['/billing/address'], 'active' => Core::checkMCA('billing', 'address', '*')],
     ],
